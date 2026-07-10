c1f25027097a909ca4837a31c566b9c0fd49d824
Jeff <jepre.dinglasan03@gmail.com>
2026-07-10 14:27:56 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+function logger(req){
+
+    const now = new Date().toLocaleDateString();
+    console.log(`[${now}] \t ${req.method} \t ${req.url}`)
+}
+
+app.use((req,res,next) =>{
+    logger(req);
+    next();
+})
+
+app.get("/", (req,res) =>{
+    res.send("hello from Express!");
+});
+
+
+app.get("/about",(req,res) =>{
+    res.send("hello from about section");
+})
+
+app.get("/api/users",(req,res) =>{
+    res.json([
+        {id:1,name:"joseph",role:"student"},
+        {id:2,name:"jeff",role:"teacher"},
+    ])
+})
+
+
+app.listen(port, () =>{
+    console.log(`Server running at http://localhost:${port}`);
+})
+
+
+
+
+
