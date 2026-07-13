948f3c56e734d1dfa5e893a31e3375aa3cd2f4b0
Jeff <jepre.dinglasan03@gmail.com>
2026-07-13 23:50:19 +0800

    learning crud

--- server.js
+++ server.js
@@ -3,37 +3,40 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-function logger(req,res,next){
+let users = [
+  { id: 1, name: "joseph", role: "student" },
+  { id: 2, name: "joan", Role: "Teacher" },
+];
 
-    const now = new Date().toLocaleDateString();
-    console.log(`[${now}] \t ${req.method} \t ${req.url}`)
-    next();
+
+function logger(req, res, next) {
+  const now = new Date().toLocaleDateString();
+  console.log(`[${now}] \t ${req.method} \t ${req.url}`);
+  next();
 }
 
 app.use(logger);
 
-app.get("/", (req,res) =>{
-    res.send("hello from Express!");
+app.get("/", (req, res) => {
+  res.send("hello from Express!");
 });
 
-~
-app.get("/about",(req,res) =>{
-    res.send("hello from about section");
-})
+app.get("/about", (req, res) => {
+  res.send("hello from about section");
+});
 
-app.get("/api/users",(req,res) =>{
-    res.json([
-        {id:1,name:"joseph",role:"student"},
-        {id:2,name:"jeff",role:"teacher"},
-    ])
-})
+app.get("/api/users", (req, res) => {
+  res.json([
+    { id: 1, name: "joseph", role: "student" },
+    { id: 2, name: "jeff", role: "teacher" },
+  ]);
+});
 
-
-app.listen(port, () =>{
-    console.log(`Server running at http://localhost:${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 
 
-
-
-
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
